400b569177ed196b7567c8e7f3537a30952a22aa
Kseniya <k89814422777@yandex.ru>
2025-05-25 19:39:18 +0300

    изменила информацию в подвале

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,9 +9,9 @@ const Footer = () => {
         <div className="footer-section">
           <h3>Контакты</h3>
           <p>Striznevo Guest House</p>
-          <p>Вологодская область, с. Стризнево</p>
-          <p>Телефон: <a href="tel:+79999999999">+7 (999) 999-99-99</a></p>
-          <p>Email: <a href="mailto:info@striznevo.ru">info@striznevo.ru</a></p>
+          <p>Вологодская область, пос. Стризнево</p>
+          <p>Телефон: <a href="tel:+79115136840">+7 (911) 513-68-40</a></p>
+          <p>Email: <a href="mailto:akvilon_35@mail.ru">akvilon_35@mail.ru</a></p>
         </div>
 
         <div className="footer-section">
